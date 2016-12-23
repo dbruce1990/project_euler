36b9ea8a8275f684dba48cd193d0e5f08c27b165
dustin bruce <dbruce1990@gmail.com>
2016-12-22 19:33:44 -0800

    trying to write tests for solution1 with mocha and chai but stuck at the moment

--- project_euler/problems/1/solution.js
+++ project_euler/problems/1/solution.js
@@ -2,26 +2,29 @@
 //
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
-
-function getMultiples(multiplicands, maxNum) {
+var Solution1 = function() {};
+Solution1.prototype.getMultiples = function(multiplicands, maxNum) {
   var multiples = [];
   for(var i = 0; i < maxNum; i++){
     multiplicands.forEach(function(multiplicand){
       if(i % multiplicand == 0)
-        if(!multiples.includes(i)){
-          multiples.push(i);
-        }
+      if(!multiples.includes(i)){
+        multiples.push(i);
+      }
     });
   }
   return multiples;
 }
 
-function getSum(nums){
+Solution1.prototype.getSum = function(nums){
   var sum = 0;
   nums.forEach(function(num){
     sum = sum + num;
   });
   return sum;
 }
+// 
+// var solution = new Solution1();
+// console.log(solution.getSum([1,2,3]));
 
 //TODO implement tests with framework
--- project_euler/test/project_euler/problems/1/solution.js
+++ project_euler/test/project_euler/problems/1/solution.js
@@ -1,8 +1,12 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
+var assert = require('chai').assert;
+
+beforeEach(function(){
+  require('../../../../problems/1/solution.js');
+  var solution = new Solution1();
+});
+
+describe('Solution 1', function() {
+  it('should return sum of all numbers', function() {
+    console.log(solution.getSum([1]));
   });
 });
